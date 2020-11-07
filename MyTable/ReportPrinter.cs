de34a6592fba6a89fac3c2063fbb38757db9b6f0
sanmar1no <sanmar1no@mail.ru>
2020-11-07 10:54:57 +0300

    Корректировка и рефакторинг классов ExcelPrinter и ReportPrinter

--- MyTable/ReportPrinter.cs
+++ MyTable/ReportPrinter.cs
@@ -1,40 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Office.Interop.Excel;
 
 namespace MyTable
 {
     class ReportPrinter
     {
         public ExcelPrinter.Company company = new ExcelPrinter.Company();
-        public ExcelPrinter.Report report = new ExcelPrinter.Report();
+        public Variables.UserKeyEnum reportKey = new Variables.UserKeyEnum();
         public DateTime dTP5 = new DateTime();
         public DateTime dTP6 = new DateTime();
-        public string arendaCB23 = "";
         private List<string> TableList = new List<string>();
+
         public ReportPrinter()
-            : this(new ExcelPrinter.Company(), new ExcelPrinter.Report(), DateTime.Now, new DateTime())
+            : this(new ExcelPrinter.Company(), DateTime.Now, new DateTime())
         {
         }
         public ReportPrinter(ExcelPrinter.Company company)
-            : this(company, new ExcelPrinter.Report(), DateTime.Now, new DateTime())
+            : this(company, DateTime.Now, new DateTime())
         {
         }
-        public ReportPrinter(ExcelPrinter.Company company, ExcelPrinter.Report report)
-            : this(company, report, DateTime.Now, new DateTime())
+        /* public ReportPrinter(ExcelPrinter.Company company, Variables.userKeyEnum report)
+             : this(company, DateTime.Now, new DateTime())
+         {
+                    this.reportKey = reportKey;
+         }*/
+        public ReportPrinter(ExcelPrinter.Company company, DateTime dTP5)
+            : this(company, dTP5, new DateTime())
         {
         }
-        public ReportPrinter(ExcelPrinter.Company company, ExcelPrinter.Report report, DateTime dTP5)
-            : this(company, report, dTP5, new DateTime())
-        {
-        }
-        public ReportPrinter(ExcelPrinter.Company company, ExcelPrinter.Report report, DateTime dTP5, DateTime dTP6)
+        public ReportPrinter(ExcelPrinter.Company company, DateTime dTP5, DateTime dTP6)
         {
             this.company = company;
-            this.report = report;
             this.dTP5 = dTP5;
             this.dTP6 = dTP6;
         }
@@ -42,54 +38,124 @@ namespace MyTable
         {
             TableList.Clear();
             TableList.AddRange(List);
-            startReport();
         }
-        private void startReport()
+        private string periodMY(DateTime dat1, DateTime dat2)
         {
-            ExcelPrinter report1 = new ExcelPrinter();
-            switch (report)
+            if (dat1 == dat2)
             {
-                case ExcelPrinter.Report.countersPeriod:                    //отчет по расходу электросчетчиков за период 
-                    report1.company = ExcelPrinter.Company.SKB;
-                    report1.report = ExcelPrinter.Report.countersPeriod;
-                    report1.dTP5 = dTP5;
-                    report1.dTP6 = dTP6;
-                    report1.arendaCB23 = arendaCB23;
-                    report1.headName();
-                    report1.bodyTable(TableList);
-                    report1.footerTableSumm("G");
-                    report1.bordersTable();
-                    report1.endSheet();
+                return MonthToStr(dat1.Month) + " " + dat1.Year.ToString() + "г.";
+            }
+            else
+            {
+                return MonthToStr(dat1.Month) + " " + dat1.Year.ToString() + "г. - " + MonthToStr(dat2.Month) + " " + dat2.Year.ToString() + "г.";
+            }
+        }
+        private string MonthToStr(int month1)
+        {
+            string retMonth = "";
+            switch (month1)
+            {
+                case 1:
+                    retMonth = "январь";
                     break;
-                case ExcelPrinter.Report.countersInventoryElectro:          //инвентаризация электросчетчиков
-                    report1.company = ExcelPrinter.Company.Impuls;
-                    report1.report = ExcelPrinter.Report.countersInventoryElectro;
-                    report1.headName();
-                    report1.bodyTable(TableList);
-                    report1.footerTableCount();
-                    report1.bordersTable();
-                    report1.endSheet();
+                case 2:
+                    retMonth = "февраль";
                     break;
-                case ExcelPrinter.Report.countersInventoryAqua:             //инвентаризация водомеров
-                    report1.company = ExcelPrinter.Company.Impuls;
-                    report1.report = ExcelPrinter.Report.countersInventoryAqua;
-                    report1.headName();
-                    report1.bodyTable(TableList);
-                    report1.footerTableCount();
-                    report1.bordersTable();
-                    report1.endSheet();
+                case 3:
+                    retMonth = "март";
                     break;
-                case ExcelPrinter.Report.countersPeriodAll:                 //Основной отчет по электроэнергии (период - месяц)
-                    report1.company = ExcelPrinter.Company.SKB;
-                    report1.report = ExcelPrinter.Report.countersPeriodAll;
-                    report1.dTP5 = dTP5;
-                    report1.headName();
-                    report1.bodyTable(TableList);
-                    report1.footerTableSumm("D");
-                    report1.bordersTable();
-                    report1.endSheet();
+                case 4:
+                    retMonth = "апрель";
+                    break;
+                case 5:
+                    retMonth = "май";
+                    break;
+                case 6:
+                    retMonth = "июнь";
+                    break;
+                case 7:
+                    retMonth = "июль";
+                    break;
+                case 8:
+                    retMonth = "август";
+                    break;
+                case 9:
+                    retMonth = "сентябрь";
+                    break;
+                case 10:
+                    retMonth = "октябрь";
+                    break;
+                case 11:
+                    retMonth = "ноябрь";
+                    break;
+                case 12:
+                    retMonth = "декабрь";
                     break;
             }
+            return retMonth;
+        }
+        public void ReportCountersPeriod(string arendaCB23) //отчет по расходу электросчетчиков за период 
+        {
+            List<string> Header = new List<string>();
+            ExcelPrinter report1 = new ExcelPrinter();
+            DateTime data = dTP6.AddMonths(1);
+            //List<string> Header = new List<string>(){ "№", "№ точки учета по договору", "№ счетчика", "Показания на  01."+dTP5.Month + "." + dTP5.Year, "Показания на 01." + data.Month + "." + data.Year, "Расч.Коэфф.", "Расход, кВт.ч."};
+            Header.Add("№ п/п");
+            Header.Add("№ точки учета по договору");
+            Header.Add("№ счетчика    ");
+            Header.Add("Показания на  01." + dTP5.Month + "." + dTP5.Year);
+            Header.Add("Показания на 01." + data.Month + "." + data.Year);
+            Header.Add("Расч.Коэфф.");
+            Header.Add("Расход, кВт.ч.");
+            report1.company = ExcelPrinter.Company.SKB;
+            report1.HeadArenda(arendaCB23);
+            report1.NameTable("Расчет количества потребленной электроэнергии за " + periodMY(dTP5, dTP6));
+            report1.HeadTable(Header);
+            report1.BodyTable(TableList);
+            report1.FooterTableSumm("G");
+            report1.BordersTable();
+            report1.EndSheet();
+        }
+        public void ReportCountersInventory(Variables.UserKeyEnum reportKey) //инвентаризация электросчетчиков/водомеров
+        {
+            List<string> Header = new List<string>();
+            ExcelPrinter report1 = new ExcelPrinter();
+            report1.company = ExcelPrinter.Company.Impuls;
+            Header.Add("№ п/п");
+            Header.Add("№ Корпуса и помещения");
+            Header.Add("№ счетчика    ");
+            Header.Add("Марка счетчика");
+            Header.Add("Год выпуска/поверки");
+            Header.Add("Показания (последние), " + (reportKey == Variables.UserKeyEnum.electro ? "кВт*ч" : "куб.м."));
+            if(reportKey == Variables.UserKeyEnum.electro) report1.NameTable("Инвентаризация электросчетчиков");
+            if(reportKey == Variables.UserKeyEnum.aqua) report1.NameTable("Инвентаризация водомеров");
+            report1.HeadTable(Header);
+            report1.BodyTable(TableList);
+            report1.FooterTableCount();
+            report1.BordersTable();
+            report1.EndSheet();
+        }
+        public void ReportCountersPeriodAll() //Основной отчет по электроэнергии (период - месяц)
+        {
+            List<string> Header = new List<string>();
+            ExcelPrinter report1 = new ExcelPrinter();
+            report1.company = ExcelPrinter.Company.SKB;
+            Header.Add("№ п/п");
+            Header.Add("Арендатор  ");
+            Header.Add("№ счетчика  ");
+            Header.Add("Расход, кВт*ч");
+            string dataStr = "";
+            if (dTP5 != new DateTime())
+            {
+                DateTime toName = dTP5;// dTP5.AddMonths(-1);
+                dataStr = MonthToStr(toName.Month) + " " + toName.Year + "г.";
+            }
+            report1.NameTable("Расчет количества потребленной электроэнергии за " + dataStr);
+            report1.HeadTable(Header);
+            report1.BodyTable(TableList);
+            report1.FooterTableSumm("D");
+            report1.BordersTable();
+            report1.EndSheet();
         }
     }
 }
