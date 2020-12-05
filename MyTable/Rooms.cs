3542c199a258639743962bc98befa57ccdb6072e
sanmar1no <sanmar1no@mail.ru>
2020-12-05 11:39:32 +0300

    rooms

---
+++ MyTable/Rooms.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MyTable
+{
+    class Rooms
+    {
+        string building;	//	№ корпуса
+        string floor;	//	№ этажа
+        string room;	//	№ помещения
+        double roomArea;	//	Площадь помещения, кв. м
+        string addressPlan;	//	Расположение  dwg планировки
+        string addressCircuitPlan;	//	Расположение  плана электросети
+        string addressCircuitLine;	//	Расположение  однолинейной схемы
+        string addressCircuitWater;	//	Расположение  плана водоснабжения
+        string addressCircuitHeat;	//	Расположение  плана теплоснабжения
+        double roomVolume;	//	Объем помещения
+        double ratioHeat;	//	Коэффициент для расчета тепла
+        Point[] coordinatesPoints;	//	Координаты расположения помещения
+    }
+}
