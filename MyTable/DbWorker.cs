d87176984ede62eaadea52494bef2a6f92c30639
IXti <ixti@example.com>
2020-12-11 22:31:50 +0300

    Добавил БД - MyTableDB, создал класс для работы с БД - DbWorker, а в нём первые запросы к БД

---
+++ MyTable/DbWorker.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace MyTable {
+    class DbWorker {
+
+        private static string dataSource = "./MyTableDB.db";
+
+        public DbWorker() {
+        }
+
+        private SqliteConnection getConnection() {
+            SqliteConnectionStringBuilder scs = new SqliteConnectionStringBuilder();
+            scs.DataSource = dataSource;
+
+            return new SqliteConnection(scs.ConnectionString);
+        }
+
+        //Метод создаёт таблицу rooms в БД, если её ещё не было
+        public void creatRoomsTable() {
+            string s = "CREATE TABLE IF NOT EXISTS rooms (" +
+                                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                "building TEXT NOT NULL," +
+                                "floor INTEGER NOT NULL," +
+                                "room TEXT NOT NULL," +
+                                "roomArea REAL NOT NULL," +
+                                "addressPlan TEXT NOT NULL," +
+                                "addressCircuitPlan TEXT NOT NULL," +
+                                "addressCircuitLine TEXT NOT NULL," +
+                                "addressCircuitWater TEXT NOT NULL," +
+                                "addressCircuitHeat TEXT NOT NULL," +
+                                "roomVolume REAL NOT NULL," +
+                                "ratioHeat REAL NOT NULL" +
+                                ");";
+
+            using (SqliteConnection connection = getConnection()) {
+                connection.Open();
+
+                SqliteCommand cmd = connection.CreateCommand();
+                cmd.CommandText = s;
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        //Метод вставляет данные в таблицу rooms из одного объекта Room
+        public void insertRoomsTable(Room r) {
+
+            string s = "INSERT INTO rooms (" +
+                "building, floor, room, roomArea, " +
+                "addressPlan, addressCircuitPlan, addressCircuitLine, " +
+                "addressCircuitWater, addressCircuitHeat, roomVolume, ratioHeat) VALUES" +
+                $"('{r.building}', '{r.floor}', '{r.room}', '{r.roomArea}', " +
+                $"'{r.addressPlan}', '{r.addressCircuitPlan}', '{r.addressCircuitLine}', " +
+                $"'{r.addressCircuitWater}', '{r.addressCircuitHeat}', '{r.roomVolume}', '{r.ratioHeat}');";
+
+            using (SqliteConnection connection = getConnection()) {
+                connection.Open();
+
+                SqliteCommand cmd = connection.CreateCommand();
+                cmd.CommandText = s;
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        //Метод вытаскивает dct данные из таблицы rooms из БД и возвращает заполненный объект List<Room>
+        public List<Room> selectRoomsTable() {
+
+            string s = "SELECT * FROM rooms;";
+
+            using (SqliteConnection connection = getConnection()) {
+                connection.Open();
+
+                SqliteCommand cmd = connection.CreateCommand();
+                cmd.CommandText = s;
+
+                List<Room> list = new List<Room>();
+                using (SqliteDataReader reader = cmd.ExecuteReader()) {
+                    while (reader.Read()) {
+
+                        Room r = new Room();
+                        r.ID = reader.GetInt32(0);
+                        r.building = reader.GetString(1);
+                        r.floor = reader.GetInt32(2);
+                        r.room = reader.GetString(3);
+                        r.roomArea = reader.GetDouble(4);
+                        r.addressPlan = reader.GetString(5);
+                        r.addressCircuitPlan = reader.GetString(6);
+                        r.addressCircuitLine = reader.GetString(7);
+                        r.addressCircuitWater = reader.GetString(8);
+                        r.addressCircuitHeat = reader.GetString(9);
+                        r.roomVolume = reader.GetDouble(10);
+                        r.ratioHeat = reader.GetDouble(11);
+
+                        list.Add(r);
+                    }
+                }
+
+                return list;
+            }
+        }
+
+
+    }
+}
